238973c8b2481237b036bb7fd16a982402bad5e2
Emine Shaban <emineshaban@icloud.com>
2023-07-28 21:33:54 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className='logo-img'>
+          <img src='./images/logo.webp' />
+        </div>
+
+        <ul>
+          <li>
+            
+          </li>
+        </ul>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
