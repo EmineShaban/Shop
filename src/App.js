63e0826ed08002eeca561e639be02ed3250189b8
Emine Shaban <emineshaban@icloud.com>
2023-07-29 19:16:00 +0300

    create product grid

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import './App.css';
 import Filters from './components/Filters';
+import Phone from './components/Phone';
+import Laptop from './components/Laptop';
+import Desktop from './components/Desktop';
+import Keyboard from './components/Keyboard';
+import Mouse from './components/Mouse';
+
+import { Link } from "react-router-dom"
+
+import { Routes, Route } from 'react-router-dom'
 import ListOfProducts from './components/ListOfProducts';
 
 function App() {
@@ -13,32 +22,50 @@ function App() {
         </div>
 
         <ul>
-          <li>
-            <a href="#home">Home</a>
-          </li>
-          <li>
-            <a href="#phone">Phone</a>
-          </li>
-          <li>
-            <a href="#laptop">Laptop</a>
-          </li>
-          <li>
-            <a href="#mouse">Mouse</a>
-          </li>
-          <li>
-            <a href="#keyboard">Keyboard</a>
-          </li>
-        </ul>
-        <div className='order'>
-          <a href='#order'>ORDER</a>
-        </div>
+        
+        <li>
+                    <Link to={'/'}><a>Home</a></Link>
+
+                </li>
+                <li>
+                    <Link to={'/phone'}><a>Phone</a></Link>
+
+                </li>
+                <li>
+                    <Link to={'/desktop'}><a>Desktop</a></Link>
+
+                </li>
+                <li>
+                    <Link to={'/laptop'}><a>Laptop</a></Link>
+
+                </li>
+                <li>
+                    <Link to={'/mouse'}><a>Mouse</a></Link>
+
+                </li>
+                <li>
+                    <Link to={'/keyboard'}><a>Keyboard</a></Link>
+
+                </li>
+                </ul>
        
       </header>
 
 
       <div className='container'>
         <Filters />
-        <ListOfProducts />
+        {/* <ListOfProducts /> */}
+        <Routes>
+            <Route path='/' element={<ListOfProducts />} />
+            <Route path='/phone' element={<Phone />} />
+            <Route path='/laptop' element={<Laptop />} />
+            <Route path='/desktop' element={<Desktop />} />
+            <Route path='/mouse' element={<Mouse />} />
+            <Route path='/keyboard' element={<Keyboard />} />
+
+            
+
+          </Routes>
       </div>
 
 
--- src/components/ListOfProducts.js
+++ src/components/ListOfProducts.js
@@ -1,12 +1,72 @@
 import './ListOfProducts.css';
- 
+import data from '../data.json'
+import { useState } from 'react';
+import Sort from './Sort';
 function ListOfProducts() {
-  return (
-    <div>
 
-    <p>aaaaaaaaaaa</p>
-    </div>
-  )
+    const productPerRow = 6;
+    const arrData= []
+    // data.map((product, index) => {
+
+    //     arrData.push(product)
+    //     console.log(data)
+    // }
+
+    // )
+
+    const [next, setNext] = useState(productPerRow);
+    const handleMoreProducts = () => {
+        console.log('aaa')
+        setNext(next + productPerRow);
+    };
+
+    return (
+        <div>
+            <div>
+                <div className='name'>
+                    <h1>ALL PRODUCTS</h1>
+                </div>
+                <div className='sort'>
+                  <Sort data={data}/>
+                </div>
+
+            </div>
+            <div className='list-of-product'>
+
+
+                {data.slice(0, next).map((product, index) => (
+                    <div className='div-product' key={index}>
+
+
+                        <h3 className='title' >{product.title}</h3>
+                        <div className='product-img'>
+
+                            <img src={product.thumbnail} alt="product" />
+                        </div>
+                        <div className='price-stars'>
+                            <p className='price'>{product.price}$</p>
+                            <p>STARS</p>
+                        </div>
+                        <p className='descr'>{product.description}</p>
+                        <br />
+                        <div className='div-order'>
+
+                            <button className='order'>ORDER NOW</button>
+                        </div>
+                    </div>
+                ))}
+
+            </div>
+            <div className='load-more-div'>
+
+                {next < data.length && (
+                    <button className="load-more" onClick={handleMoreProducts} >Load more</button>
+
+                )}
+            </div>
+
+        </div>
+    )
 }
 
 
